3ebcf8f57881d705c890f4457513e646fff6a70a
krishgupta1 <krishgupta11110@gmail.com>
2023-03-30 02:32:50 +0530

    All done but Issue in Design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Testimonial from "./components/Testimonial"
+import reviews from "./data"
 
 const App = () => {
-  return <div>App</div>;
+  return(
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center item-center bg-gray-200">
+
+      <div className="text-center">
+        
+        <h1 className="text-4xl font-bold">Our Testimonials</h1>
+        <div className="bg-violet-500 h-[4px] w-1/5 mt-1 mx-auto"></div>
+        <Testimonial  className="flex flex-col"
+          reviews={reviews}/>
+      </div>
+    </div>
+  )
 };
 
 export default App;
